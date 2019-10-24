aa17a50422a25e4360ec6a693b018343d6e994c7
tarun <tarunkumar.as@outlook.com>
2019-10-24 04:20:49 -0400

    Added game logic for all the files

--- chess/lib/chess/Pieces/knight.ex
+++ chess/lib/chess/Pieces/knight.ex
@@ -1,5 +1,7 @@
 defmodule Chess.Knight do
 
+alias Chess.BoardStatus
+
   @pieces %{
     wKing: 0,
     wQueen: 1,
@@ -17,11 +19,154 @@ defmodule Chess.Knight do
   }
 
   def playmove(game,position) do
-  	IO.inspect("Playmove")
+	IO.inspect("Play move")
+  	selectedPiece = game.board[String.to_atom(game.selectedPiece)]
+  	tempBoard = Map.put(game.board, String.to_atom(game.selectedPiece), @pieces.empty)
+  	newBoard = Map.put(tempBoard, String.to_atom(position), selectedPiece)
+  	
+  	%{
+  		board: newBoard,
+      	selectedPiece: -1,
+      	availableMoves: [],
+      	isWhiteTurn: !game.isWhiteTurn
+  	}
   end
   
   def getMoves(game,position) do
+  	IO.inspect("Get move")
+  	isWhite = isWhiteColor(game,position)
+  	  	
+  	if game.isWhiteTurn == true do
+  		getMovesForWhitePiece(game, position)
+  	else
+  		getMovesForBlackPiece(game, position)
+  	end
+  end
   
+  defp isWhiteColor(game, position) do
+  	currentSelection = if (game.selectedPiece == -1) do
+  		game.board[String.to_atom(position)]
+  	else 
+  		game.selectedPiece
+  	end
+  	currentSelection == @pieces.wKnight
+  end
+  
+  defp getMovesForWhitePiece(game, position) do
+  	characterPosition = String.at(position,0)
+  	{numberPosition, ""} = Integer.parse(String.at(position,1))
+  	<<nextChar::utf8>> = characterPosition
+  	
+  	ul1 = List.to_string([nextChar - 1]) <> Integer.to_string(numberPosition - 2)
+  	ul2 = List.to_string([nextChar - 2]) <> Integer.to_string(numberPosition - 1)
+  	ur1 = List.to_string([nextChar + 1]) <> Integer.to_string(numberPosition - 2)
+  	ur2 = List.to_string([nextChar + 2]) <> Integer.to_string(numberPosition - 1)
+  	bl1 = List.to_string([nextChar - 1]) <> Integer.to_string(numberPosition + 2)
+  	bl2 = List.to_string([nextChar - 2]) <> Integer.to_string(numberPosition + 1)
+  	br1 = List.to_string([nextChar + 1]) <> Integer.to_string(numberPosition + 2)
+  	br2 = List.to_string([nextChar + 2]) <> Integer.to_string(numberPosition + 1)
+  	
+  	availableMoves =
+  		if Map.has_key?(game.board, String.to_atom(ul1)) && game.board[String.to_atom(ul1)] > 5 && BoardStatus.isCheckWhenMovingPiece() == false do
+  			[ul1]
+  		else 
+  			[]
+  		end ++ if Map.has_key?(game.board, String.to_atom(ul2)) && game.board[String.to_atom(ul2)] > 5 && BoardStatus.isCheckWhenMovingPiece() == false do
+  			[ul2]
+  		else 
+  			[]
+  		end ++ if Map.has_key?(game.board, String.to_atom(ur1)) && game.board[String.to_atom(ur1)] > 5 && BoardStatus.isCheckWhenMovingPiece() == false do
+  			[ur1]
+  		else 
+  			[]
+  		end ++ if Map.has_key?(game.board, String.to_atom(ur2)) && game.board[String.to_atom(ur2)] > 5 && BoardStatus.isCheckWhenMovingPiece() == false do
+  			[ur2]
+  		else 
+  			[]
+  		end ++ if Map.has_key?(game.board, String.to_atom(bl1)) && game.board[String.to_atom(bl1)] > 5 && BoardStatus.isCheckWhenMovingPiece() == false do
+  			[bl1]
+  		else 
+  			[]
+  		end ++ if Map.has_key?(game.board, String.to_atom(bl2)) && game.board[String.to_atom(bl2)] > 5 && BoardStatus.isCheckWhenMovingPiece() == false do
+  			[bl2]
+  		else 
+  			[]
+  		end ++ if Map.has_key?(game.board, String.to_atom(br1)) && game.board[String.to_atom(br1)] > 5 && BoardStatus.isCheckWhenMovingPiece() == false do
+  			[br1]
+  		else 
+  			[]
+  		end ++ if Map.has_key?(game.board, String.to_atom(br2)) && game.board[String.to_atom(br2)] > 5 && BoardStatus.isCheckWhenMovingPiece() == false do
+  			[br2]
+  		else 
+  			[]
+  		end
+  		
+  		IO.inspect(availableMoves)
+  	%{
+  		board: game.board,
+      	selectedPiece: position,
+      	availableMoves: availableMoves,
+      	isWhiteTurn: game.isWhiteTurn
+  	}
+  end
+  
+  defp getMovesForBlackPiece(game, position) do
+  	characterPosition = String.at(position,0)
+  	{numberPosition, ""} = Integer.parse(String.at(position,1))
+  	<<nextChar::utf8>> = characterPosition
+  	
+  	ul1 = List.to_string([nextChar - 1]) <> Integer.to_string(numberPosition - 2)
+  	ul2 = List.to_string([nextChar - 2]) <> Integer.to_string(numberPosition - 1)
+  	ur1 = List.to_string([nextChar + 1]) <> Integer.to_string(numberPosition - 2)
+  	ur2 = List.to_string([nextChar + 2]) <> Integer.to_string(numberPosition - 1)
+  	bl1 = List.to_string([nextChar - 1]) <> Integer.to_string(numberPosition + 2)
+  	bl2 = List.to_string([nextChar - 2]) <> Integer.to_string(numberPosition + 1)
+  	br1 = List.to_string([nextChar + 1]) <> Integer.to_string(numberPosition + 2)
+  	br2 = List.to_string([nextChar + 2]) <> Integer.to_string(numberPosition + 1)
+  	
+  	availableMoves =
+  		if Map.has_key?(game.board, String.to_atom(ul1)) && (game.board[String.to_atom(ul1)] <= 5 || game.board[String.to_atom(ul1)] == 12) && BoardStatus.isCheckWhenMovingPiece() == false do
+  			[ul1]
+  		else 
+  			[]
+  		end ++ if Map.has_key?(game.board, String.to_atom(ul2)) && (game.board[String.to_atom(ul2)] <= 5 || game.board[String.to_atom(ul2)] == 12) && BoardStatus.isCheckWhenMovingPiece() == false do
+  			[ul2]
+  		else 
+  			[]
+  		end ++ if Map.has_key?(game.board, String.to_atom(ur1)) && (game.board[String.to_atom(ur1)] <= 5 || game.board[String.to_atom(ur1)] == 12) && BoardStatus.isCheckWhenMovingPiece() == false do
+  			[ur1]
+  		else 
+  			[]
+  		end ++ if Map.has_key?(game.board, String.to_atom(ur2)) && (game.board[String.to_atom(ur2)] <= 5 || game.board[String.to_atom(ur2)] == 12) && BoardStatus.isCheckWhenMovingPiece() == false do
+  			[ur2]
+  		else 
+  			[]
+  		end ++ if Map.has_key?(game.board, String.to_atom(bl1)) && (game.board[String.to_atom(bl1)] <= 5 || game.board[String.to_atom(bl1)] == 12) && BoardStatus.isCheckWhenMovingPiece() == false do
+  			[bl1]
+  		else 
+  			[]
+  		end ++ if Map.has_key?(game.board, String.to_atom(bl2)) && (game.board[String.to_atom(bl2)] <= 5 || game.board[String.to_atom(bl2)] == 12) && BoardStatus.isCheckWhenMovingPiece() == false do
+  			[bl2]
+  		else 
+  			[]
+  		end ++ if Map.has_key?(game.board, String.to_atom(br1)) && (game.board[String.to_atom(br1)] <= 5 || game.board[String.to_atom(br1)] == 12) && BoardStatus.isCheckWhenMovingPiece() == false do
+  			[br1]
+  		else 
+  			[]
+  		end ++ if Map.has_key?(game.board, String.to_atom(br2)) && (game.board[String.to_atom(br2)] <= 5 || game.board[String.to_atom(br2)] == 12) && BoardStatus.isCheckWhenMovingPiece() == false do
+  			[br2]
+  		else 
+  			[]
+  		end
+  	
+  	IO.inspect(availableMoves)
+  	
+  	%{
+  		board: game.board,
+      	selectedPiece: position,
+      	availableMoves: availableMoves,
+      	isWhiteTurn: game.isWhiteTurn
+  	}
   end
   
 end
--- chess/lib/chess/Pieces/rook.ex
+++ chess/lib/chess/Pieces/rook.ex
@@ -1,5 +1,7 @@
 defmodule Chess.Rook do
 
+alias Chess.BoardStatus
+
   @pieces %{
     wKing: 0,
     wQueen: 1,
@@ -17,11 +19,159 @@ defmodule Chess.Rook do
   }
 
   def playmove(game,position) do
-  	IO.inspect("Playmove")
+	IO.inspect("Play move")
+  	selectedPiece = game.board[String.to_atom(game.selectedPiece)]
+  	tempBoard = Map.put(game.board, String.to_atom(game.selectedPiece), @pieces.empty)
+  	newBoard = Map.put(tempBoard, String.to_atom(position), selectedPiece)
+  	
+  	%{
+  		board: newBoard,
+      	selectedPiece: -1,
+      	availableMoves: [],
+      	isWhiteTurn: !game.isWhiteTurn
+  	}
   end
   
   def getMoves(game,position) do
+  	IO.inspect("Get move")
+  	isWhite = isWhiteColor(game,position)
+  	  	
+  	availableMoves = getTValidMoves(game, position, [], isWhite) ++ getRValidMoves(game, position, [], isWhite) ++ getLValidMoves(game, position, [], isWhite) ++ getBValidMoves(game, position, [], isWhite)
+  	
+  	IO.inspect(availableMoves)
+  	
+  	%{
+  		board: game.board,
+      	selectedPiece: position,
+      	availableMoves: availableMoves,
+      	isWhiteTurn: game.isWhiteTurn
+  	}
+  	
+  end
   
+  defp isWhiteColor(game, position) do
+  	currentSelection = if (game.selectedPiece == -1) do
+  		game.board[String.to_atom(position)]
+  	else 
+  		game.selectedPiece
+  	end
+  	currentSelection == @pieces.wRook
+  end
+  
+  defp getLValidMoves(game, currentPosition, moves, isWhite) do
+	characterPosition = String.at(currentPosition,0)
+  	{numberPosition, ""} = Integer.parse(String.at(currentPosition,1))
+  	<<nextChar::utf8>> = characterPosition
+  	newPosition = List.to_string([nextChar - 1]) <> Integer.to_string(numberPosition)
+  	
+  	if Map.has_key?(game.board, String.to_atom(newPosition)) do
+  		if game.board[String.to_atom(newPosition)] == 12 do
+  			if BoardStatus.isCheckWhenMovingPiece() == false do
+  				getLValidMoves(game, newPosition, moves ++ [newPosition], isWhite)
+  			else
+  				getLValidMoves(game, newPosition, moves, isWhite)
+  			end
+  		else
+  			if (isWhite == true && game.board[String.to_atom(newPosition)] < 6) || (isWhite == false && game.board[String.to_atom(newPosition)] > 5) do
+  				moves
+  			else
+  				if BoardStatus.isCheckWhenMovingPiece() == false do
+  					moves ++ [newPosition]
+  				else
+  					moves
+  				end
+  			end
+  		end
+  	else
+  		moves
+  	end
+  end
+  
+  defp getRValidMoves(game, currentPosition, moves, isWhite) do
+  	characterPosition = String.at(currentPosition,0)
+  	{numberPosition, ""} = Integer.parse(String.at(currentPosition,1))
+  	<<nextChar::utf8>> = characterPosition
+  	newPosition = List.to_string([nextChar + 1]) <> Integer.to_string(numberPosition)
+  	
+  	if Map.has_key?(game.board, String.to_atom(newPosition)) do
+  		if game.board[String.to_atom(newPosition)] == 12 do
+  			if BoardStatus.isCheckWhenMovingPiece() == false do
+  				getRValidMoves(game, newPosition, moves ++ [newPosition], isWhite)
+  			else
+  				getRValidMoves(game, newPosition, moves, isWhite)
+  			end
+  		else
+  			if (isWhite == true && game.board[String.to_atom(newPosition)] < 6) || (isWhite == false && game.board[String.to_atom(newPosition)] > 5) do
+  				moves
+  			else
+  				if BoardStatus.isCheckWhenMovingPiece() == false do
+  					moves ++ [newPosition]
+  				else
+  					moves
+  				end
+  			end
+  		end
+  	else
+  		moves
+  	end
+  end
+  
+  defp getTValidMoves(game, currentPosition, moves, isWhite) do
+  	characterPosition = String.at(currentPosition,0)
+  	{numberPosition, ""} = Integer.parse(String.at(currentPosition,1))
+  	<<nextChar::utf8>> = characterPosition
+  	newPosition = List.to_string([nextChar]) <> Integer.to_string(numberPosition - 1)
+  	
+  	if Map.has_key?(game.board, String.to_atom(newPosition)) do
+  		if game.board[String.to_atom(newPosition)] == 12 do
+  			if BoardStatus.isCheckWhenMovingPiece() == false do
+  				getTValidMoves(game, newPosition, moves ++ [newPosition], isWhite)
+  			else
+  				getTValidMoves(game, newPosition, moves, isWhite)
+  			end
+  		else
+  			if (isWhite == true && game.board[String.to_atom(newPosition)] < 6) || (isWhite == false && game.board[String.to_atom(newPosition)] > 5) do
+  				moves
+  			else
+  				if BoardStatus.isCheckWhenMovingPiece() == false do
+  					moves ++ [newPosition]
+  				else
+  					moves
+  				end
+  			end
+  		end
+  	else
+  		moves
+  	end
+  end
+  
+  defp getBValidMoves(game, currentPosition, moves, isWhite) do
+  	characterPosition = String.at(currentPosition,0)
+  	{numberPosition, ""} = Integer.parse(String.at(currentPosition,1))
+  	<<nextChar::utf8>> = characterPosition
+  	newPosition = List.to_string([nextChar]) <> Integer.to_string(numberPosition + 1)
+  	
+  	if Map.has_key?(game.board, String.to_atom(newPosition)) do
+  		if game.board[String.to_atom(newPosition)] == 12 do
+  			if BoardStatus.isCheckWhenMovingPiece() == false do
+  				getBValidMoves(game, newPosition, moves ++ [newPosition], isWhite)
+  			else
+  				getBValidMoves(game, newPosition, moves, isWhite)
+  			end
+  		else
+  			if (isWhite == true && game.board[String.to_atom(newPosition)] < 6) || (isWhite == false && game.board[String.to_atom(newPosition)] > 5) do
+  				moves
+  			else
+  				if BoardStatus.isCheckWhenMovingPiece() == false do
+  					moves ++ [newPosition]
+  				else
+  					moves
+  				end
+  			end
+  		end
+  	else
+  		moves
+  	end
   end
   
 end
